85d9ca1db4d7a47b4ff6d04338e32c7946bf164e
Ashwini Bagadi <ashwinibagadi4@gmail.com>
2025-12-11 21:29:16 +0530

    Practice git

---
+++ Trial_files/Arithmetic.py
@@ -0,0 +1,5 @@
+def add(n1, n2):
+    print("Addition: ",n1+n2)
+
+def sub(n1,n2):
+    print("Subtraction: ",n1-n2)
---
+++ Trial_files/main.py
@@ -0,0 +1,10 @@
+import Arithmetic
+
+num1=int(input("Enter Num1: "))
+num2=int(input("Enter Num2: "))
+
+Arithmetic.add(num1,num2)
+Arithmetic.sub(num1,num2)
+
+Arithmetic.add(10,5)
+Arithmetic.sub(10,5)
