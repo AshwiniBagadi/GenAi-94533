3724cbd319fd3aa6f4c66a51f1a35ea2a1e520ab
Ashwini Bagadi <ashwinibagadi4@gmail.com>
2025-12-22 09:15:54 +0530

    Day-06 demos

---
+++ Day06/demo02.py
@@ -0,0 +1,50 @@
+from langchain.chat_models import init_chat_model
+from langchain.agents import create_agent
+from langchain.tools import tool
+
+@tool
+def calculator(expression):
+    """
+    This calculator function sloves any arithmetic expression 
+    containing all constant values.
+    It supports basic arithmetic operators +,-,*,/ and parenthesis.
+    
+    :param expression: str input arithmetic expression
+    :returns expression result as str
+    """
+    try:
+        result= eval(expression)
+        return str(result)
+    except:
+        return "Error: Cannot solve expression!"
+    
+# Create model
+llm=init_chat_model(
+    model = "google/gemma-3-4b",
+    model_provider = "openai",
+    base_url = "http://127.0.0.1:1234/v1",
+    api_key = "non-needed"
+)
+
+# Create agent
+agent= create_agent(
+    model=llm,
+    tools=[
+        calculator
+    ],
+    system_prompt="You are a helpful Assistant. Answer in short."
+)
+
+while True:
+    user_input=input("You: ")
+    if user_input=="exit":
+        break
+
+    result=agent.invoke({
+        "messages": [
+            {"role": "user", "content": user_input}
+        ]
+    })
+    llm_output= result["messages"][-1]
+    print("AI: ",llm_output.content)
+
---
+++ Day06/demo04.py
@@ -0,0 +1,53 @@
+from langchain.chat_models import init_chat_model
+from langchain.agents import create_agent
+from langchain.agents.middleware import wrap_model_call
+
+@wrap_model_call
+def model_logging(request, handler):
+    print("Before model call: ",'-'*20)
+    response=handler(request)
+    print("After model call: ",'-'*20)
+
+    response.result[0].content=response.result[0].content.upper()
+
+    return response
+
+
+@wrap_model_call
+def limit_model_context(request, handler):
+    print("*Before model call: ",'-'*20)
+
+    request.messages=request.messages[-5:]
+    response=handler(request)
+    response.result[0].content = response.result[0].content.upper()
+    return response
+
+llm =init_chat_model(
+    model = "google/gemma-3-4b",
+    model_provider = "openai",
+    base_url ="http://127.0.0.1:1234/v1",
+    api_key ="non-needed"
+)
+
+conversation=[]
+
+agent = create_agent(model=llm, 
+            tools=[],
+            middleware=[model_logging, limit_model_context],
+            system_prompt="You are a helpful assistant. Answer in short."
+        )
+
+while True:
+    # take user input
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    # append user message in coversation
+    conversation.append({"role": "user", "content": user_input})
+    # invoke the agent
+    result = agent.invoke({"messages": conversation})
+    # print the result's last message
+    ai_msg = result["messages"][-1]
+    print("AI: ", ai_msg.content)
+    # let's use conversation history returned by agent
+    conversation = result["messages"]
